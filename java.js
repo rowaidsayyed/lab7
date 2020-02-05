d5cadd2e3cb4eac028a3d3a8b909010c1b1dc71e
rowidsayyed <rowidsayyed@gmail.com>
2020-02-05 20:37:56 +0200

    update javascript file

--- java.js
+++ java.js
@@ -3,13 +3,17 @@ alert("Welcome 102d5 ");
 var name= prompt("Please enter your name");
 var age= prompt("Please enter your age");
 
-if(age>19){
-    var majot= prompt("Please enter your major");
-    var text =name + major;
+if(age>=19 && age <27){
+    var major= prompt("Please enter your major");
+    var text =name +" with major "+ major;
+}
+else if(age>=27){
+    var job= prompt("Please enter your job");
+    var text =name +" with job "+ job;
 }
 else{
     var school= prompt("Please enter your school");
-    var text =name + school;
+    var text =name +" from school "+ school;
 }
 
 return text;
